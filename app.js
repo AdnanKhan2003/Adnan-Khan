7e205ca7c0c449376b516d9961bc8e66d7da9b3d
Adnan Khan <adnanmkhan2003@gmail.com>
2023-07-28 17:57:12 +0530

    Refactored some HTML, CSS and Js file

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// I] VARIABLES
 // NAVBAR BTNS
 const navbar = document.querySelector(".navbar");
 const navLinks = document.querySelectorAll(".nav-link");
 const navItems = document.querySelector(".nav-items-group");
+const navHeight = navbar.getBoundingClientRect().height;
 
 // BUTTONS
 const btnHamburger = document.querySelector(".hamburger");
@@ -21,33 +24,24 @@ const sectionEducation = document.querySelector(".education");
 
 // ELEMENTS
 const dots = document.querySelectorAll(".dot");
+const overlay = document.querySelector(".blur-wrapper");
+const mode = document.querySelector(".mode-swap");
+const modeImg = document.querySelector(".mode-swap-icon");
 
-// REVEAL elements on scroll
-// -> Callback function
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// II] FUNCTIONS
+
+// FUNCITONALITY - REVEAL elements on scroll
 const slideEle = function (entries, observer) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) return;
-
   entry.target.classList.remove("hidden");
 
   observer.unobserve(entry.target);
 };
-// Intersection Observer with callback function and options object
-const sectionObserver = new IntersectionObserver(slideEle, {
-  root: null,
-  threshold: 0.15,
-});
 
-// Observing target intersection with viewport
-sectionsAll.forEach((section) => {
-  sectionObserver.observe(section);
-  section.classList.add("hidden");
-});
-
-// STICKY navbar
-const navHeight = navbar.getBoundingClientRect().height;
-
+// FUNCITONALITY - STICKY navbar
 const stickyNav = function (entries) {
   const [entry] = entries;
 
@@ -58,15 +52,58 @@ const stickyNav = function (entries) {
   }
 };
 
-// OBSERVING navbar
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-  rootMargin: `${-navHeight}px`,
-});
-headerObserver.observe(sectionHeader);
+// FUNCITONALITY - OPEN NAVBAR
+const openNav = (e) => {
+  e.preventDefault();
+  navItems.style.left = "0";
 
-// //
+  overlay.classList.remove("d-none");
+  overlay.classList.add("overlay");
+  // document.querySelector("body").style.overflowY = "hidden";
+  document.querySelector("body").classList.remove("enable-scroll");
+  document.querySelector("body").classList.add("stop-scroll");
+};
+
+// FUNCITONALITY - CLOSE NAVBAR
+const closeNav = (e) => {
+  e.preventDefault();
+
+  overlay.classList.remove("overlay");
+  overlay.classList.add("d-none");
+  // document.querySelector("body").style.overflowY = "scroll";
+  document.querySelector("body").classList.remove("stop-scroll");
+  document.querySelector("body").classList.add("enable-scroll");
+  navItems.style.left = "-700px";
+};
+
+// FUNCTIONALITY - TOGGLE DARK & LIGHT MODE
+const toggleMode = function (e) {
+  e.preventDefault();
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    modeImg.src = "img/icon-2.png";
+    document.body.style.backgroundColor = "var(--bg-bd-clr)";
+  } else {
+    document.body.style.backgroundColor = "#fff6f6";
+    modeImg.src = "img/icon-1.png";
+  }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// EVENT LISTENERS
+btnHamburger.addEventListener("click", openNav);
+btnClose.addEventListener("click", closeNav);
+overlay.addEventListener("click", closeNav);
+mode.addEventListener("click", toggleMode);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+closeNav();
+
+// SMOOTH SCROLL ON NAVBAR
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", function (e) {
     e.preventDefault();
@@ -75,45 +112,30 @@ navLinks.forEach((navLink) => {
       const clicked = e.target;
       const id = clicked.getAttribute("href");
 
+      navItems.style.left = "-700px";
+      // document.body.style.overflowY = "scroll";
+      document.body.classList.remove("stop-scroll");
+      document.body.classList.add("enable-scroll");
+      overlay.classList.remove("overlay");
       document.querySelector(`${id}`).scrollIntoView({ behavior: "smooth" });
     }
   });
 });
-const overlay = document.querySelector(".blur-wrapper");
-const openNav = (e) => {
-  e.preventDefault();
-  navItems.style.left = "0";
 
-  overlay.classList.add("overlay");
-  document.querySelector("body").style.overflowY = "hidden";
-  // overlay.style.di;
-};
-
-const closeNav = (e) => {
-  e.preventDefault();
-  overlay.classList.remove("overlay");
-  document.querySelector("body").style.overflowY = "scroll";
-  navItems.style.left = "-700px";
-};
-
-btnHamburger.addEventListener("click", openNav);
-btnClose.addEventListener("click", closeNav);
-overlay.addEventListener("click", closeNav);
-
-const mode = document.querySelector(".mode-swap");
-const modeImg = document.querySelector(".mode-swap img");
-console.log(modeImg);
-
-sectionsAll.forEach((section) => {
-  if (section % 2 === 0) {
-    // section.c
-  }
+// STICKY navbar
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${-navHeight}px`,
 });
-const lightMode = function () {};
+headerObserver.observe(sectionHeader);
 
-mode.addEventListener("click", function (e) {
-  e.preventDefault();
-  // let counter =
-  // modeImg.src = `icon-${}`;
-  document.body.classList.toggle("dark-mode");
+// REVEAL elements onscroll
+const sectionObserver = new IntersectionObserver(slideEle, {
+  root: null,
+  threshold: 0.1,
+});
+sectionsAll.forEach((section) => {
+  section.classList.add("hidden");
+  sectionObserver.observe(section);
 });
